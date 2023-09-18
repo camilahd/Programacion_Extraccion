2aa9183958526464c7730ce6edd691adba20e74c
camilhd <hernandez.camila@uabc.edu.mx>
2023-09-17 20:54:24 -0700

    (:

--- Meta 1.4.py
+++ Meta 1.4.py
@@ -1,6 +1,5 @@
 #+Camila Elizabeth Hernandez Alaniz
 # 951
-# 10/09/23
 
 
 import time
@@ -10,9 +9,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By # queda pendiente
 from webdriver_manager.chrome import ChromeDriverManager
-#from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
 
-def busqueda_amazon(producto_buscar, producto_cantidad):
+
+def busqueda_amazon(producto_buscar, pestañas):
     s = Service(ChromeDriverManager().install())
     opc = Options()
     opc.add_argument("--window-size= 1020, 1200")
@@ -29,27 +29,37 @@ def busqueda_amazon(producto_buscar, producto_cantidad):
     datos = { "Nombre": [], "Rating": [], "Precio": [], "Fecha de entrega": []}
     time.sleep(2)
 
-
-    for i in range(producto_cantidad):
+    for h in range(pestañas):
         nombres = amazon.find_elements(By.CLASS_NAME, 'a-size-base-plus.a-color-base.a-text-normal')
-        ratings = amazon.find_elements(By.CLASS_NAME, 'a-size-base.puis-bold-weight-text')
+        ratings = amazon.find_elements(By.CLASS_NAME, 'a-size-base.puis-bold-weight-text') #cambia constantemente y en este momento no tiene como tal el numero de las estrellas amazon
         precios = amazon.find_elements(By.CLASS_NAME, 'a-price')
         fechas_entrega = amazon.find_elements(By.CLASS_NAME, 'a-color-base.a-text-bold')
-        # Añade la información del producto actual a los datos
-        datos["Nombre"].append(nombres[i].text)
 
-        if len(ratings) > 0:
-            datos["Rating"].append(ratings[i].text)
-        else:
-            datos["Rating"].append("None")
-        precio_text = precios[i].text
-        precio_numero = precio_text.replace("$", "")
-
-        if len(precio_numero) > 0:
-            datos["Precio"].append(precio_numero)
-        else:
-            datos["Precio"].append("None")
-        datos["Fecha de entrega"].append(fechas_entrega[i].text)
+        for i in range(len(nombres)):
+            datos["Nombre"].append(nombres[i].text)
+            if len(ratings) > i:
+                datos["Rating"].append(ratings[i].text)
+            else:
+                datos["Rating"].append("None")
+            precio_text = precios[i].text
+            precio_numero = precio_text.replace("$", "")
+            if len(precio_numero) > 0:
+                datos["Precio"].append(precio_numero)
+            else:
+                datos["Precio"].append("None")
+            if len(fechas_entrega) > 0:
+                datos["Fecha de entrega"].append(fechas_entrega[i].text)
+            else:
+                datos["Fecha de entrega"].append("None")
+        try:
+            boton_next = amazon.find_element(By.CLASS_NAME, 's-pagination-item.s-pagination-button')
+            boton_next.click()
+            time.sleep(3)
+        #except NoSuchElementException:
+            #print("El botón 'Siguiente' no se encontró en la página.")
+            #break
+        except:
+            break
 
     amazon.close()
     datos_df = pd.DataFrame(datos)
@@ -58,5 +68,5 @@ def busqueda_amazon(producto_buscar, producto_cantidad):
     return datos_df
 
 producto_buscar = input("Ingrese el producto que desea buscar:")
-producto_cantidad = int(input("¿Cuantos pestañas de productos desea buscar?"))
-busqueda_amazon(producto_buscar, producto_cantidad)
+pestañas = int(input("¿Cuantos pestañas de productos desea buscar?"))
+busqueda_amazon(producto_buscar, pestañas)
